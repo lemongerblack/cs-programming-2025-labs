9072ead23eab914d815db62909239346543bf738
lemongerblack <lemongerblack@gmail.com>
2025-09-30 13:21:27 +1000

    Create 6.py

---
+++ labs/lab3/6.py
@@ -0,0 +1,14 @@
+n = int(input("введите число:"))
+i, fa = 0, [0, 1, 1]
+f = 0
+while f < n and f != n:
+    if i >= 3:
+        f = fa[i-2] + fa[i-1]
+        if f > n:
+            break
+        fa.append(f)
+        print(f)
+    elif f <= 1:
+        f = fa[i]
+        print(f)
+    i += 1
