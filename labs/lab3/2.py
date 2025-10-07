192aa5d02d635ca071065245bf65dc7d36c2ce25
lemongerblack <lemongerblack@gmail.com>
2025-10-07 12:16:19 +1000

    исправление недочёта

--- labs/lab3/2.py
+++ labs/lab3/2.py
@@ -1,3 +1,3 @@
-ch = int(input())
+ch = int(input("Введите число от 1 до 9:"))
 for i in range(1, 11):
     print(f"{i}: {ch} * {i} = {ch * i}")
--- labs/lab3/4.py
+++ labs/lab3/4.py
@@ -1,4 +1,4 @@
-n, f = int(input()), 1
+n, f = int(input("Введите число, чтобы расчитать факториал:")), 1
 for i in range(1, n + 1):
     f *= i
 print(f)
