d847724c4d1a7f967bd975ae62d79e1fc297cae3
lemongerblack <lemongerblack@gmail.com>
2025-11-04 14:04:09 +1000

    у

--- labs/lab4/7.py
+++ labs/lab4/7.py
@@ -1,14 +1,3 @@
-#ch = sorted(map(int, (input("Введите три чесла через прообел: ")).split()))
-#print(ch[0])
-
 ch = input("Введите три чесла через пробел: ")
-a = [int(i) for i in ch.split()]
-for i in range(len(a) - 1):
-    f = 0
-    for j in range(len(a) - i - 1):
-        if a[j] > a[j + 1]:
-            f += 1
-            a[j], a[j + 1] = a[j + 1], a[j]
-    if f == 0:
-        break
+a = sorted([int(i) for i in ch.split()])
 print(a[0])
