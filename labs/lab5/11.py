0b2586278956378c2cb48a8cc5d08e66170b9b33
lemongerblack <lemongerblack@gmail.com>
2025-12-02 12:03:02 +1000

    ю

--- labs/lab5/11.py
+++
@@ -1,7 +0,0 @@
-import requests from bs4 import BeautifulSoup  
-url = "https://news.ycombinator.com/"  
-response = requests.get(url)  
-soup = BeautifulSoup(response.text, 'lxml')  
-titles = soup.find_all('a', class_='storylink')  
-for title in titles:  
-    print(title.text)
