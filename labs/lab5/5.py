06185a8de009b4a5d8c15ecf48e800cb772f0c85
lemongerblack <lemongerblack@gmail.com>
2025-12-09 12:04:02 +1000

    .

--- labs/lab5/5.py
+++ labs/lab5/5.py
@@ -3,5 +3,5 @@ s = {}
 for i in range(0, len(a) - 1, 2):
     if a[i] not in s:
         s[a[i]] = int(a[i + 1])
-print(f"Максимальная цена у товара: {max(s, key = s.get)}, цена: {s.get(max(s, key = s.get))}")
+print(f"Максимальная цена у товара: {max(s, key = s.get)}, цена: {s.get(max(s, key = s.get))}   {max(s)}")
 print(f"Минимальная цена у товара: {min(s, key = s.get)}, цена: {s.get(min(s, key = s.get))}")
