0121039180fabe82381191cfe8ac8fc1bc9a6a7e
lemongerblack <lemongerblack@gmail.com>
2025-12-16 19:10:48 +1000

    т

--- labs/lab6/1.py
+++ labs/lab6/1.py
@@ -4,7 +4,7 @@ a = input("Введите время для перевода_").split()
 hmc = [1, 60, 3600]
 h, m, c = 0, 1, 2
 t = int(a[0][:-1])
-if eval(a[0][-1]) <= eval(a[-1]):
+if eval(a[0][-1]) <= eval(a[-1]) and a[0][-1] != "m":
     print(t * hmc[eval(a[-1])])
 elif a[0][-1] == "m":
     if a[-1] == "h":
