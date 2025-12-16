0490821f351059546e3acdcf03798cfe15b4a4e1
lemongerblack <lemongerblack@gmail.com>
2025-12-16 13:08:22 +1000

    12qrwfswfsfwfwfwfwfwfwfwfwfwf

--- labs/lab6/2.py
+++ labs/lab6/2.py
@@ -1,16 +1,14 @@
 print("Перевод времени без смс и регистрации(:")
 print("пример ввода: 10h m", "c - секунды", "m - минуты", "h - часы", sep="\n")
 a = input("Введите время для перевода_").split()
-hmc = [1, 60, 3600]
-h, m, c = 0, 1, 2
+h = 1
+m = 60
+c = 3600
 t = int(a[0][:-1])
-if eval(a[0][-1]) <= eval(a[-1]):
-    print(t * hmc[eval(a[-1])])
-elif a[0][-1] == "m":
-    if a[-1] == "h":
-        print(t / 60)
-    else:
-        print(t * 60)
+if eval(a[0][-1]) < eval(a[-1]):
+    print(eval(a[0][-1]) * eval(a[-1]))
+elif eval(a[0][-1]) == eval(a[-1]):
+    print(a[0][-1])
 else:
-    hmc = hmc[::-1]
-    print(t / hmc[eval(a[-1]) ])
+    print(eval(a[0][-1]) / eval(a[-1]))
+print(a, eval(a[-1]), a[-1])
