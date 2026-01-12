7e1a5b20b30eb9197b989db5a5fb5effd8a5df12
lemongerblack <lemongerblack@gmail.com>
2026-01-12 20:10:43 +1000

    /

---
+++ labs/lab7/3.py
@@ -0,0 +1,13 @@
+personnel = [
+    {"name": "Dr. Klein", "clearance": 2},
+    {"name": "Agent Brooks", "clearance": 4},
+    {"name": "Technician Reed", "clearance": 1}
+]
+
+categpers = list(map(lambda x: {"name": x["name"],
+                                 "clearance": x["clearance"],
+                                   "category": "Restricted" if x["clearance"] == 1
+                                     else "Confidential" if 2 <= x["clearance"] <= 3 
+                                     else "Top Secret"},
+                                       personnel))
+print(*categpers)
