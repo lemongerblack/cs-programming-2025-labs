874ee84ea2f14f8fa62a088f73945389b0b714a5
lemongerblack <lemongerblack@gmail.com>
2026-01-19 15:26:41 +1000

    д

--- labs/lab7/3.py
+++ labs/lab7/3.py
@@ -6,8 +6,10 @@ personnel = [
 
 categpers = list(map(lambda x: {"name": x["name"],
                                  "clearance": x["clearance"],
-                                   "category": "Restricted" if x["clearance"] == 1
-                                     else "Confidential" if 2 <= x["clearance"] <= 3 
+                                   "category": "Restricted"
+                                     if x["clearance"] == 1
+                                     else "Confidential" 
+                                     if 2 <= x["clearance"] <= 3 
                                      else "Top Secret"},
                                        personnel))
-print(*categpers)
+print(categpers)
