3af4a6b385ba3035c930aa3c6b02150d257004bd
lemongerblack <lemongerblack@gmail.com>
2026-01-25 00:46:19 +1000

    в

--- project/CharacterCreation.py
+++ project/CharacterCreation.py
@@ -1,8 +1,10 @@
 from random import randint
+
 def СreatingСharacteristics():
     name = input('введите имя персонажа_')
+    flag = True
     print('Выберите расу', '===============', '1 - человек', '2 - эльф', '3 - дворф', sep='\n')
-    while True:
+    while flag:
         n = input('введите ваш выбор_')
         if n == '1':
             HP = randint(20, 35)
@@ -12,7 +14,8 @@ def СreatingСharacteristics():
             Attentiveness = randint(10, 20)
             Height = randint(152, 184)
             Weight = Height - 90
-            return {'name': name, 'r': 'ЧЕЛОВЕК','hp': HP, 'at': Attack, 'def': Defense, 'agl': Agility, 'att': Attentiveness, 'hei': Height, 'wei': Weight}
+            return {'ИМЯ': name, 'РАСА': 'ЧЕЛОВЕК','ЗДОРОВЬЕ': HP, 'СИЛА_АТАКИ': Attack, 'ЗАЩИТА': Defense,
+                     'ЛОВКОСТЬ': Agility, 'ВНИМАТЕЛЬНОСТЬ': Attentiveness, 'РОСТ': Height, 'ВЕС': Weight}
             break
         elif n == '2':
             HP = randint(20, 30)
@@ -22,7 +25,8 @@ def СreatingСharacteristics():
             Attentiveness = randint(10, 20)
             Height = randint(150, 185)
             Weight = Height - 100
-            return {'name': name, 'r': 'ЭЛЬФ','hp': HP, 'at': Attack, 'def': Defense, 'agl': Agility, 'att': Attentiveness, 'hei': Height, 'wei': Weight}
+            return {'ИМЯ': name, 'РАСА': 'ЭЛЬФ','ЗДОРОВЬЕ': HP, 'СИЛА_АТАКИ': Attack, 'ЗАЩИТА': Defense,
+                     'ЛОВКОСТЬ': Agility, 'ВНИМАТЕЛЬНОСТЬ': Attentiveness, 'РОСТ': Height, 'ВЕС': Weight}
             break
         elif n == '3':
             HP = randint(30, 45)
@@ -32,8 +36,8 @@ def СreatingСharacteristics():
             Attentiveness = randint(13, 20)
             Height = randint(122, 152)
             Weight = Height - 75
-            return {'name': name, 'r': 'ДВОРФ','hp': HP, 'at': Attack, 'def': Defense, 'agl': Agility, 'att': Attentiveness, 'hei': Height, 'wei': Weight}
+            return {'ИМЯ': name, 'РАСА': 'ДВОРФ','ЗДОРОВЬЕ': HP, 'СИЛА_АТАКИ': Attack, 'ЗАЩИТА': Defense,
+                     'ЛОВКОСТЬ': Agility, 'ВНИМАТЕЛЬНОСТЬ': Attentiveness, 'РОСТ': Height, 'ВЕС': Weight}
             break
         else:
             print("ОШИБКА: выберите число от 1 до 3, чтобы выбрать расу")
-print(СreatingСharacteristics())
