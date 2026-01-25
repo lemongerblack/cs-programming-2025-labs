05033d39988d02baa4478057c5556a9d8aa3b89b
lemongerblack <lemongerblack@gmail.com>
2026-01-25 20:16:59 +1000

    /

--- project/FloorGeneration.py
+++ project/FloorGeneration.py
@@ -4,15 +4,21 @@ from FirstLevelVrgi import *
 def roommon():
     n = []
     for i in range(1, randint(1, 2) + 1):
-        n.append(Monstors1[0])
-    print(n)
+        n.append(Monstors1[randint(0, 5)])
     return n
+
+def roomchil():
+    return randint(5, 20)
+
+def roomgold():
+    pass
 
 def roomlvl1():
     print('Вы подымаетесь выше и вот развилка...')
+    var1 = view[randint(0, 3)]
+    var2 = view[randint(0, 3)]
+    print('Куда вы пройдете дальше?', '=========================', f'1 - комната {var1}', f'2 - комната {var2}', sep='\n')
 
-
-    print('Куда вы пройдете дальше?', '====================', '1 - ', '2 -', sep='\n')
 
 view = ['монстров', 'отдыха', 'сокровищь', 'ловушка']
 
