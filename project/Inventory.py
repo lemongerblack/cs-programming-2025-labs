e784291ae93846ded321a1917b8feef1f988869b
lemongerblack <lemongerblack@gmail.com>
2026-01-26 03:01:16 +1000

    /

---
+++ project/Inventory.py
@@ -0,0 +1,47 @@
+from Game import *
+from Items import *
+
+iventory = {'1': [], '2': [], '3': [], '4': [], '5': []}
+equipment = []
+
+def prinvetory():
+    print(f"Ваш ивентарь {iventory}", f"Ваша экиперовка {equipment}", sep='\n', end='\n', flush=True, delay=0.1)
+
+def use():
+    while True:
+        var = input("Выберите какой предмет вы хотите использовать_ ")
+        try:
+            itemsuse = iventory[var][0]
+            if len(iventory[var]) > 0:
+                if 'a' in iventory[var][0]:
+                    if iventory[var][0]['a'] + player['ЗДОРОВЬЕ'] <= mxhp:
+                        player['ЗДОРОВЬЕ'] = player['ЗДОРОВЬЕ'] + iventory[var][0]['a']
+                        iventory[var] = []
+                        prinvetory()
+                    else:
+                        player['ЗДОРОВЬЕ'] = mxhp
+                        iventory[var] = []
+                        prinvetory()
+                elif 'b' in iventory[var][0]:
+                    if len[equipment] == 0:
+                        equipment.append(iventory[var][0])
+                        player['ЗАЩИТА'] = player['ЗАЩИТА'] + iventory[var][0]['b']
+                        iventory[var] = []
+                        prinvetory()
+                    else:
+                        print(f"Вы хотите поменять {equipment(0)} на {iventory[var][0]}", "1 - Да", "2 - нет", sep="\n", end="\n", flush=True, delay=0.1)
+                        while True:
+                            playerotv = input("Введите ваш вариант")
+                            if playerotv == '1':
+                                equipment = [iventory[var][0]]
+                                player['ЗАЩИТА'] = mxdef + iventory[var][0]['b']
+                                iventory[var] = []
+                                prinvetory()
+                                break
+                            elif playerotv == '2':
+                                break
+                            else:
+                                print("Выберите возможный вариант 1 или 2")
+                
+        except:
+            pass 
