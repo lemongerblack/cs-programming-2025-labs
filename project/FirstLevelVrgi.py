354d6f2057ae0b3650f5c59d024c84ecf2319150
lemongerblack <lemongerblack@gmail.com>
2026-01-26 14:08:09 +1000

    d

--- project/FirstLevelVrgi.py
+++ project/FirstLevelVrgi.py
@@ -11,20 +11,23 @@ class mon1:
     def Attacks(self, player, evade):
         kick = randint(1, 20)
         damager = randint(1, self.Damage)
-        if ((kick + (self.Attack // 10)) > player["ЗАЩИТА"]) or ((kick + (self.Attack // 10)) > evade):
+        if (evade > 0) and ((kick + (self.Attack // 10)) > evade):
+            player["ЗДОРОВЬЕ"] = player["ЗДОРОВЬЕ"] - damager
+            print(f"Вы получили - {damager} урона")
+        elif ((kick + (self.Attack // 10)) > player["ЗАЩИТА"]):
             player["ЗДОРОВЬЕ"] = player["ЗДОРОВЬЕ"] - damager
             print(f"Вы получили - {damager} урона")
         else:
             print("Вы избежали урона! Это отличный шанс!")
     
-    def TakingDamage(self, playeratack, damage):
+    def TakingDamage(self, playeratack : int, damage : int):
         if (playeratack > self.Defense) and (self.HP - damage != 0):
             self.HP = self.HP - damage
-        elif self.HP - damage <= 0:
+        elif (playeratack > self.Defense) and (self.HP - damage <= 0):
             self.HP = 0
             print("Вы отдалели эту тварь!")
         else:
-             print("Вы начинаете отаковать и, к сожаление промахиваетесь")
+             print("Вы начинаете атаковать и, к сожаление промахиваетесь")
 
 Monstors1 = [{'ИМЯ': 'багбир', 'СТАТЫ': [20, 20, 7, 13, 7]}, {'ИМЯ': 'темная-слизь', 'СТАТЫ': [10, 11, 5, 15, 4]},
              {'ИМЯ': 'нисшая-тень', 'СТАТЫ': [16, 20, 6, 20, 5]}, {'ИМЯ': 'грязевой-монстр', 'СТАТЫ': [14, 14, 10, 5, 5]},
