2f95c1773e90fa89ea20a47634a3ac3cd8ad48fb
lemongerblack <lemongerblack@gmail.com>
2026-01-29 21:19:01 +1000

    в

--- project/GAMEs.py
+++ project/GAMEs.py
@@ -34,7 +34,7 @@ def praction():
 def fight(lvlfloor):
     global pointr
     global lvlpr
-    for _ in range(1, randint(2, 3) + (lvlfloor - 1)):
+    for _ in range(1, randint(2, 4)):
         if player['ЗДОРОВЬЕ'] <= 0:
             continue
         monstor = roommon()
